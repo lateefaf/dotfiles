e2a1ff1731cb3db053889961cfc2600559c8d0d3
Lateef <alateef4@gmail.com>
2024-09-17 10:01:03 -0400

    Add fixes to i3 config
    Change i3lock to black bg
    Change resize keybinds for width

--- i3/.config/i3/lockscreen.sh
+++ i3/.config/i3/lockscreen.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Directory containing images
-IMAGE_DIR="/home/lateef/Pictures//wallpapers/fallout_loading_screens/"
-
-# Select a random image from the directory
-RANDOM_IMAGE=$(find "$IMAGE_DIR" -type f | shuf -n 1)
-
-# Use the image for the i3lock background
-i3lock -i "$RANDOM_IMAGE"
+# # Directory containing images
+# IMAGE_DIR="/home/lateef/Pictures//wallpapers/fallout_loading_screens/"
+#
+# # Select a random image from the directory
+# RANDOM_IMAGE=$(find "$IMAGE_DIR" -type f | shuf -n 1)
+#
+# # Use the image for the i3lock background
+# i3lock -i "$RANDOM_IMAGE"
+i3lock --color 000000
 
 # If you want to blur the image, you might use Imagemagick like this:
 # convert "$RANDOM_IMAGE" -blur 0x8 /tmp/i3lock.png
